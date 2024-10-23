b9db8a9e3ccb50272e06e7501cc483eb7fa81a16
tuco5 <tudor.comparan@gmail.com>
2024-10-23 17:31:10 -0600

    add i18n without routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { DarkModeToggle } from "./_components/DarkModeToggle";
+import { useTranslations } from "next-intl";
 
 export default function HomePage() {
+  const t = useTranslations("HomePage");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center dark:bg-stone-900 dark:text-white">
       <h1 className="text-5xl">
         block<span className="text-orange-500">spot</span>
       </h1>
-      <p>Under construction</p>
+      <p>{t("hero")}</p>
       <DarkModeToggle />
     </main>
   );
---
+++ src/i18n/request.ts
@@ -0,0 +1,13 @@
+import { getRequestConfig } from "next-intl/server";
+
+export default getRequestConfig(async () => {
+  // Provide a static locale, fetch a user setting,
+  // read from `cookies()`, `headers()`, etc.
+  const locale = "en";
+  const messages = await import(`../../messages/${locale}.json`);
+
+  return {
+    locale,
+    messages: messages.default,
+  };
+});
