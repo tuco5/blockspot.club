a506b8f7e3bae51ee44a81941f2f74f59baa2097
tuco5 <tudor.comparan@gmail.com>
2024-10-23 16:29:15 -0600

    fix hydration error

--- src/app/_components/DarkModeToggle.tsx
+++ src/app/_components/DarkModeToggle.tsx
@@ -2,13 +2,21 @@
 import { useTheme } from "next-themes";
 import { Toggle } from "@/components/ui/toggle";
 import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export function DarkModeToggle() {
   const { theme, setTheme } = useTheme();
 
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   function toggleDarkMode(isDark: boolean) {
     setTheme(isDark ? "dark" : "ligth");
-    console.log({ isDark, theme });
   }
 
   return (
@@ -16,9 +24,9 @@ export function DarkModeToggle() {
       onPressedChange={toggleDarkMode}
       variant="outline"
       className="flex w-16 justify-start rounded-full p-1.5 transition-all duration-300 dark:justify-end"
-      defaultPressed={theme === "dark"}
+      pressed={theme ? theme === "dark" : false}
     >
-      <div className="relative flex rounded-full bg-slate-200/30 p-1.5">
+      <div className="relative flex rounded-full bg-slate-300 p-1.5 dark:bg-slate-200/30">
         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-300 dark:absolute dark:-rotate-90 dark:scale-0" />
         <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-300 dark:static dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,12 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
     </ThemeProvider>
   );
