63dce128dbf93187b1109ee2851be19f980acc7b
tuco5 <tudor.comparan@gmail.com>
2024-10-23 13:48:12 -0600

    dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { DarkModeToggle } from "./_components/DarkModeToggle";
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center dark:bg-stone-900 dark:text-white">
       <h1 className="text-5xl">
         block<span className="text-orange-500">spot</span>
       </h1>
       <p>Under construction</p>
+      <DarkModeToggle />
     </main>
   );
 }
