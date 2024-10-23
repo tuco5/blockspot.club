9070f1493721449c88921e4ec56ee10f82365f5e
tuco5 <tudor.comparan@gmail.com>
2024-10-22 18:53:25 -0600

    creat a draft logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      <h1 className="text-5xl">blockspot</h1>
+      <h1 className="text-5xl">
+        block<span className="text-orange-500">spot</span>
+      </h1>
       <p>Under construction</p>
     </main>
   );
