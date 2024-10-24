6050d4d24934cbe9b6b271bf7d4c48455a37ac1c
tuco5 <tudor.comparan@gmail.com>
2024-10-24 08:42:11 -0600

    add more config and cookie setters and getters for locale

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,9 +1,9 @@
+import { getUserLocale } from "@/server/services/locale";
 import { getRequestConfig } from "next-intl/server";
 
-export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = "en";
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await getUserLocale();
+
   const messages = await import(`../../messages/${locale}.json`);
 
   return {
