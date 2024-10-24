aaf1df6118f1d0092c85deb660a8d41f33e6ff6c
tuco5 <tudor.comparan@gmail.com>
2024-10-24 10:33:16 -0600

    fix any error

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -4,10 +4,8 @@ import { getRequestConfig } from "next-intl/server";
 export default getRequestConfig(async () => {
   const locale = await getUserLocale();
 
-  const messages = await import(`../../messages/${locale}.json`);
-
   return {
     locale,
-    messages: messages.default,
+    messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
