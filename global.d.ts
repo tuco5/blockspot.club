1f9cb249b5d31aa86a501915c78cc82e3fc0940d
tuco5 <tudor.comparan@gmail.com>
2024-10-24 15:50:02 -0600

    create general prop types

--- global.d.ts
+++ global.d.ts
@@ -3,5 +3,14 @@ import es from "./messages/es.json";
 type Messages = typeof es;
 
 declare global {
+  // general use
+  interface Props {
+    className?: string;
+  }
+  interface PropsWithChildren extends Props {
+    children?: React.ReactNode;
+  }
+
+  // i18n
   interface IntlMessages extends Messages {}
 }
